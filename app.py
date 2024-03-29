fcd5ae4f4019d77175c0e88b2dd1d10dd4d0e4fc
Chanuka Dulanjaya <86533953+chanuka-dulanjaya@users.noreply.github.com>
2024-03-30 00:34:32 +0530

    Update app.py
    port changed

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def capture_photo():
     return 'Photo captured successfully!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
